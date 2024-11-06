8bfc8d473806aa386e67c0fff7db7441544c2e6a
Muhammad Kharisma Azhari <kharisma.azhari02@gmail.com>
2024-11-06 07:24:49 +0700

    Add server.

--- ch-simple/simple/src/main/java/org/sonatype/mavenbook/App.java
+++ ch-simple/simple/src/main/java/org/sonatype/mavenbook/App.java
@@ -1,13 +1,35 @@
 package org.sonatype.mavenbook;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello World!");
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+
+        server.createContext("/", new HttpHandler() {
+            @Override
+            public void handle(HttpExchange exchange) throws IOException {
+                String response = "Hello from the server!";
+                exchange.sendResponseHeaders(200, response.length());
+                OutputStream os = exchange.getResponseBody();
+                os.write(response.getBytes());
+                os.close();
+            }
+        });
+
+        server.start();
+        System.out.println("Server started on port 8000");
     }
 }
